912e20f9845d38082414760152e03d071438206e
zhang0135789 <zz634682577@163.com>
2020-05-19 11:41:32 +0800

    init 工厂设计模式

---
+++ factory/src/main/java/com/pattern/shape/Rectangle.java
@@ -0,0 +1,15 @@
+package com.pattern.shape;
+
+/**
+ * @Author: zz
+ * @Description:
+ * @Date: 11:40 AM 5/19/20
+ * @Modified By
+ */
+public class Rectangle implements Shape {
+
+    @Override
+    public void draw() {
+
+    }
+}
---
+++ factory/src/main/java/com/pattern/shape/Shape.java
@@ -0,0 +1,11 @@
+package com.pattern.shape;
+
+/**
+ * @Author: zz
+ * @Description:
+ * @Date: 11:38 AM 5/19/20
+ * @Modified By
+ */
+interface Shape {
+    void draw();
+}
