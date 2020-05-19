69c4f80f0f67de96da55710f782434b037772c88
zhang0135789 <zz634682577@163.com>
2020-05-19 14:48:54 +0800

    init 工厂设计模式

--- factory/src/main/java/com/pattern/shape/Circle.java
+++ factory/src/main/java/com/pattern/shape/Circle.java
@@ -6,10 +6,10 @@ package com.pattern.shape;
  * @Date: 11:40 AM 5/19/20
  * @Modified By
  */
-public class Rectangle implements Shape {
+public class Circle implements Shape {
 
     @Override
     public void draw() {
-        System.out.println("Inside Rectangle::draw() method.");
+        System.out.println("Inside Circle::draw() method.");
     }
 }
---
+++ factory/src/main/java/com/pattern/shape/ShapeFactory.java
@@ -0,0 +1,25 @@
+package com.pattern.shape;
+
+/**
+ * @Author: zz
+ * @Description:
+ * @Date: 2:38 PM 5/19/20
+ * @Modified By
+ */
+public class ShapeFactory {
+
+    //使用 getShape 方法获取形状类型的对象
+    public Shape getShape(String shapeType){
+        if(shapeType == null){
+            return null;
+        }
+        if(shapeType.equalsIgnoreCase("CIRCLE")){
+            return new Circle();
+        } else if(shapeType.equalsIgnoreCase("RECTANGLE")){
+            return new Rectangle();
+        } else if(shapeType.equalsIgnoreCase("SQUARE")){
+            return new Square();
+        }
+        return null;
+    }
+}
